235561116a67a74e579000e733a6586b7e4cbe33
rohan <rohanpurr@gmail.com>
2018-10-18 14:19:18 -0400

    adding static route and postbuild script

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const validateSearchSmiles = require('./validation/searchValidation')
 
 const {
@@ -25,8 +26,9 @@ const {
 const app = express();
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 5000;
-app.listen(port, ()=>console.log(`app listening on port ${port}`));
+
+
+
 
 app.post('/', (req,res)=>{
     console.log(req.body.chems);
@@ -97,3 +99,14 @@ app.post('/', (req,res)=>{
         })
         .then(canonical=>res.json(canonical));*/
 });
+
+if (process.env.NODE_ENV==='production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res)=>{
+        res.sendFile(path.join(__dirname,'client','build','index.html'));
+    });
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, ()=>console.log(`app listening on port ${port}`));
